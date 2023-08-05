626fb68407027687c41077d7f5310e557376b6ac
Jedi66 <Jedi66@gmx.de>
2023-08-05 08:54:25 +0200

    Map details now also work with the texture.

--- UoFiddler.Controls/Forms/MapDetailsForm.cs
+++ UoFiddler.Controls/Forms/MapDetailsForm.cs
@@ -9,7 +9,9 @@
  *
  ***************************************************************************/
 
+using System;
 using System.Drawing;
+using System.Windows.Controls;
 using System.Windows.Forms;
 using Ultima;
 using UoFiddler.Controls.Classes;
@@ -28,7 +30,7 @@ namespace UoFiddler.Controls.Forms
             Icon = Options.GetFiddlerIcon();
             TopMost = true;
 
-            Tile currentTile = currentMap.Tiles.GetLandTile(point.X, point.Y);
+            Tile currentTile = currentMap.Tiles.GetLandTile(point.X, point.Y);            
             richTextBox.AppendText($"X: {point.X} Y: {point.Y}\n\n");
             richTextBox.AppendText("LandTile:\n");
             richTextBox.AppendText($"{TileData.LandTable[currentTile.Id].Name}: 0x{currentTile.Id:X} Altitude: {currentTile.Z}\n\n");
@@ -40,18 +42,9 @@ namespace UoFiddler.Controls.Forms
                 ushort id = @static.Id;
                 richTextBox.AppendText($"{TileData.ItemTable[id].Name}: 0x{id:X} Hue: {@static.Hue} Altitude: {@static.Z}\n");
             }
-            // Update texture information
-            if (Textures.TestTexture(point.X) && Textures.TestTexture(point.Y))
-            {
-                Bitmap currentTexture = Textures.GetTexture(point.X);
-                if (currentTexture != null)
-                {
-                    richTextBox.AppendText("Texture:\n");
-                    richTextBox.AppendText($"Width: {currentTexture.Width} Height: {currentTexture.Height}\n");
-                    richTextBox.AppendText($"Hex-Adresse: 0x{point.X:X}\n\n");
-                    pictureBox3.Image = currentTexture;
-                }
-            }
+            // Display for the textures that are being determined.
+            UpdateTextureInformation(point, currentTile);
+
             // Display of the land tiles in pictureBox1
             Bitmap landTileBitmap = Art.GetLand(currentTile.Id);
             pictureBox1.Image = landTileBitmap;
@@ -62,6 +55,35 @@ namespace UoFiddler.Controls.Forms
                 HuedTile currentStatic = staticsAtPoint[_currentStaticIndex];
                 Bitmap staticBitmap = Art.GetStatic(currentStatic.Id);
                 pictureBox2.Image = staticBitmap;
+            }
+        }
+
+        private void UpdateTextureInformation(Point point, Tile currentTile)
+        {
+            // Get Tile ID from CurrentTile
+            int tileID = currentTile.Id;
+
+            // Determine via TileData TexID
+            int texID = TileData.LandTable[tileID].TextureId;
+
+            // Load texture
+            Bitmap texture = Textures.GetTexture(texID);
+
+            if (texture != null)
+            {
+                // Empty line 
+                richTextBox.AppendText("\n");
+                // Texture heading bold
+                richTextBox.SelectionFont = new Font(richTextBox.Font, FontStyle.Bold);
+                // Details with line breaks
+                richTextBox.AppendText("Texture:\n");
+                richTextBox.SelectionFont = new Font(richTextBox.Font, FontStyle.Regular);
+                richTextBox.AppendText($"Width: {texture.Width} Height: {texture.Height}\n");
+                richTextBox.AppendText($"TexID: 0x{texID:X}\n");
+
+
+                // Load into PictureBox
+                pictureBox3.Image = texture;
             }
         }
 
