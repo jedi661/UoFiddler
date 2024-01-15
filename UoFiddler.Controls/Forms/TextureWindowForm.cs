d2346813135f0e7da21f68b5017712039b22c87d
Jedi66 <Jedi66@gmx.de>
2024-01-15 19:39:54 +0100

    LICENSE

--- UoFiddler.Controls/Forms/TextureWindowForm.cs
+++ UoFiddler.Controls/Forms/TextureWindowForm.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿/***************************************************************************
+ *
+ * $Author: Nikodemus
+ *
+ * "THE WINE-WARE LICENSE"
+ * As long as you retain this notice you can do whatever you want with
+ * this stuff. If we meet some day, and you think this stuff is worth it,
+ * you can buy me a Wine in return.
+ *
+ ***************************************************************************/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
